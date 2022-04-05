11a162d410fe56da0cf15acbcfa86606c7404b9e
Amit Patel <89689746+technostuf@users.noreply.github.com>
2022-04-05 14:40:39 +0530

    react google map

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GoogleMapComponent from './GoogleMapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleMapComponent />
+    </div>
+  );
+}
+
+export default App;
